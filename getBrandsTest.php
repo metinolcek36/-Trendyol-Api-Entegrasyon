31841af8233188e7df4b680f5e63955b76a0b2fe
halebeyaz <50142492+halebeyaz@users.noreply.github.com>
2020-08-21 17:54:26 +0300

    Delete getBrandsTest.php

--- getBrandsTest.php
+++
@@ -1,9 +0,0 @@
-<?php 
-   include("../vendor/autoload.php");
-   include("../trendyol_entegrasyon/Api.php");
-
-
-   $trendyol = new Trendyol();
-   $getBrands=$trendyol->getBrands(100, 0);
-
-   echo($getBrands);
