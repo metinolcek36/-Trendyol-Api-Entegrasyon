e719764ebd0119d33a6aca57fe06422888a3e1cb
halebeyaz <50142492+halebeyaz@users.noreply.github.com>
2020-08-21 15:50:53 +0300

    Delete producs.php

--- producs.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$urunler= array('items'=> array('barcode'=>'123', 'brand'=>'apple',
-'attributes'=>array( array('attributeName'=>"color",'attributeValue'=>"red"))));
-echo json_encode($urunler);
