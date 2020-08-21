0a4a7fb3109fa5fe19c11fe806b49aca3a5c6819
halebeyaz <50142492+halebeyaz@users.noreply.github.com>
2020-08-21 15:51:00 +0300

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
- require 'vendor/autoload.php';
-use App\SQLiteConnection;
-
-$pdo = (new SQLiteConnection())->connect();
-if ($pdo != null)
-    echo 'successfully!';
-else
-    echo 'dont connection database!';
