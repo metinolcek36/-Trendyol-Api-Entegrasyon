cad529ab9e884ca015c59d883d427462a87d6c1d
halebeyaz <50142492+halebeyaz@users.noreply.github.com>
2020-08-22 11:28:18 +0300

    Update updateProductsTest.php

--- vendor/updateProductsTest.php
+++ vendor/updateProductsTest.php
@@ -1,88 +1,75 @@
+
 <?php 
-   include("../vendor/autoload.php");
-   include("../trendyol_entegrasyon/Api.php");
+   include('../vendor/autoload.php');
+   include('../trendyol_entegrasyon/Api.php');
 
 
    $trendyol = new Trendyol();
 
-
-   $json_object = array('item'=> array(array(
-    'barcode'=>"barkod-1234",
-    'title'=>"Bebek Takımı Pamuk",
-    'productMainId'=>"1234BT",
-    'brandId'=> 1791,
-    'categoryId'=> 411,
-    'quantity'=> 100,
-    'stockCode'=>"STK-345",
-    'dimensionalWeight'=> 2,
-  "description"=>"Ürün açıklama bilgisi",
-  "currencyType"=> "TRY",
-  "listPrice"=> 250.99,
-  "salePrice"=>120.99,
-  "vatRate"=> 18,
-  "cargoCompanyId"=>10,
-  "shipmentAddressId"=>0,
-  "returningAddressId"=> 0,
-  "deliveryDuration"=> 10,
-    'images'=>array(array('url'=>"https://www.sampleadress/path/folder/image_1.jpg")),
-    'attributes'=>array(array(
-    'attributeId'=>338,'attributeValueId'=>6980),array(
-    'attributeId'=>343,'attributeValueId'=>4294),array(
-    'attributeId'=>346,'attributeValueId'=>4290)),
-    
-    
-    'productMainId'=>"IphoneX",
-    'quantity'=>9,
-    'salePrice'=>5649.00,
-    'stockCode'=>"IphoneX",
-    'title'=>"Apple iPhone X 64 GB Cep Telefonu",
-    'variantAttributes'=>array(array('attributeName'=>"Red",'attributeValue'=>"gümüş")),
-    'vatRate'=>18
-    ) ),
-    );  
-    
    
-    //$json_str = json_encode($json_object, JSON_PRETTY_PRINT);
-    //echo $json_str;
 
-    //$oJson = json_decode($json_str) ;
+//NOT1=> Bu servis üzerinden ürünün sadece bilgileri güncellenmektedir. 
+//Stok ve fiyat değerleri güncellenmeyecektir. 
+//Ürünlerinize ait kategori bilgisini sisteme girdikten sonra değiştiremezsiniz.
+//Ürünlere ait marka bilgisini ürünleriniz onaylanana kadar değiştirebilirsiniz. 
+//Onaylandıktan sonra marka bilginizi değiştiremezsiniz.
 
-    $data = array('item'=> array(array(
-        'barcode'=>"barkod-1234",
-        'title'=>"Bebek Takımı Pamuk",
-        'productMainId'=>"1234BT",
-        'brandId'=> 1791,
-        'categoryId'=> 411,
-        'quantity'=> 100,
-        'stockCode'=>"STK-345",
-        'dimensionalWeight'=> 2,
-      "description"=>"Ürün açıklama bilgisi",
-      "currencyType"=> "TRY",
-      "listPrice"=> 250.99,
-      "salePrice"=>120.99,
-      "vatRate"=> 18,
-      "cargoCompanyId"=>10,
-      "shipmentAddressId"=>0,
-      "returningAddressId"=> 0,
-      "deliveryDuration"=> 10,
-        'images'=>array(array('url'=>"https://www.sampleadress/path/folder/image_1.jpg")),
-        'attributes'=>array(array(
-        'attributeId'=>338,'attributeValueId'=>6980),array(
-        'attributeId'=>343,'attributeValueId'=>4294),array(
-        'attributeId'=>346,'attributeValueId'=>4290)),
-        
-        
-        'productMainId'=>"IphoneX",
-        'quantity'=>9,
-        'salePrice'=>5649.00,
-        'stockCode'=>"IphoneX",
-        'title'=>"Apple iPhone X 64 GB Cep Telefonu",
-        'variantAttributes'=>array(array('attributeName'=>"Red",'attributeValue'=>"gümüş")),
-        'vatRate'=>18
-        ) ),
-        );  
-       
-        //echo json_encode($data, JSON_PRETTY_PRINT);
-        echo $trendyol->updateProduct($data, "create");
+//Yeni kategori ve kategori özellik değerleri eklenebileceği sebebiyle ürün güncellemelerinizden önce kullandığınız 
+//kategori ve kategori özellik değerlerinin güncel olup olmadığını 
+//getCategoryTree ve getCategoryAttributes servislerinden kontrol etmenizi öneririz.
 
-       
+
+$updatedData = '{
+    "items": [
+        {
+            "barcode": "barkod-1111",
+            "title": "Bebek Takımı Pamuk",
+            "productMainId": "1234BT",
+            "brandId": 1791,
+            "categoryId": 411,
+            "stockCode": "STK-123",
+            "dimensionalWeight": 12,
+            "description": "Ürün açıklama bilgisi",
+            "vatRate": 0,
+            "images": [
+                {
+                    "url": "https://www.sampleadress/path/folder/image_1.jpg"
+                }
+            ],
+            "attributes": [
+                {
+                    "attributeId": 338,
+                    "attributeValueId": 6980
+                },
+                {
+                    "attributeId": 343,
+                    "attributeValueId": 4294
+                },
+                {
+                    "attributeId": 47,
+                    "customAttributeValue": "Attribute özelliği(text olarak girebilirsiniz.)"
+                }
+            ],
+            "cargoCompanyId": 10,
+            "shipmentAddressId": 0,
+            "returningAddressId": 0
+        }
+    ]
+}';
+
+$json = json_decode($updatedData, true);
+
+$elementcount = count($json);
+echo $elementcount . "<br/>";
+$i = 0;
+foreach($json as $elem)  {
+    while($i<$elementcount){
+        echo($elem[$i]['barcode'] . "<br/>" );
+        echo ($trendyol->updateProduct($elem[0], 'update'));
+        $i+=1;
+    }
+     
+}
+
+
+
